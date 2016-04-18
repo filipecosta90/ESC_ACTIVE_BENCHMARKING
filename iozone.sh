7407458df34ed0131abd2b499957fd5f425a7d92
filipecosta90 <filipecosta.90@gmail.com>
2016-04-18 20:00:36 +0100

    added iozone write report

--- iozone.sh
+++ iozone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-/opt/csw/bin/iozone -+u -R -i 0 -S 20480 -s 1g -b /export/home/a57816/ESC_ACTIVE_BENCHMARKING_HOME/teste_write.xls -l 1 -u 1 
+/opt/csw/bin/iozone -+u -R -i 0 -S 20480 -s 39g -b /export/home/a57816/ESC_ACTIVE_BENCHMARKING_HOME/teste_write.xls -l 1 -u 1 
 
