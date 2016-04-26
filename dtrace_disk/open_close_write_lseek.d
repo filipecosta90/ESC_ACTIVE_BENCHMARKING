aae14c2c4dcd87fda5c13343856fc00f810d648a
filipecosta90 <filipecosta.90@gmail.com>
2016-04-26 03:20:21 +0100

    refined dtrace script

--- dtrace_disk/open_close_write_lseek.d
+++ dtrace_disk/open_close_write_lseek.d
@@ -7,29 +7,32 @@ dtrace:::BEGIN{
 
 }
 
-syscall::open*:entry{
+syscall::openat:entry{
+  wall_time_in = timestamp;
+  self->time_in = timestamp;
   self->pathname = copyinstr(arg1);
 }
 
-syscall::open*:return
+syscall::openat:return
 /strstr(self->pathname,"iozone.DUMMY.0") != NULL /
 {
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  self->time_in = 0;
   flag = 1;
 }
 
-syscall::read*:entry
+syscall::read:entry
 /flag == 1/
 {
   self->time_in = timestamp;
   @total_r_size[opp_num] =  sum (arg2/1024);
 }
 
-syscall::read*:return
-/flag == 1/
+syscall::read:return
+/flag == 1 && self->time_in /
 {
-  self->time_out = timestamp;
-  self->total_time = self->time_out - self->time_in;
-  @total_time[opp_num] = sum  (self->total_time);
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  self->time_in = 0;
 }
 
 syscall::fdsync:entry
@@ -40,31 +43,30 @@ syscall::fdsync:entry
 }
 
 syscall::fdsync:return
-/flag == 1/
+/flag == 1 && self->time_in /
 {
-  self->time_out = timestamp;
-  self->total_time = self->time_out - self->time_in;
-  @total_time[opp_num] = sum  (self->total_time);
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  self->time_in = 0;
 }
 
 
-syscall::write*:entry
+syscall::write:entry
 /flag == 1/
 {
   self->time_in = timestamp;
-  @total_w_size[opp_num] =  sum  (arg2/1024);
+  @total_w_size[opp_num] =  sum (arg2/1024);
+  self->time_in = 0;
 }
 
-syscall::write*:return
-/flag == 1/
+syscall::write:return
+/flag == 1 && self->time_in /
 {
-  self->time_out = timestamp;
-  self->total_time = self->time_out - self->time_in;
-  @total_time[opp_num] =  sum (self->total_time);
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  self->time_in = 0;
 }
 
 syscall::lseek:entry
-/flag == 1/
+/flag == 1 /
 {
   self->time_in = timestamp;
   @lseek_offset[opp_num] = quantize (arg1);
@@ -72,24 +74,33 @@ syscall::lseek:entry
 }
 
 syscall::lseek:return
-/flag == 1/
+/flag == 1 && self->time_in /
 {
-  self->time_out = timestamp;
-  self->total_time = self->time_out - self->time_in;
-  @total_time[opp_num] =  sum (self->total_time);
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  self->time_in = 0;
+}
+
+syscall::close:entry
+/flag == 1 /
+{
+  self->time_in = timestamp;
 }
 
 
-syscall::close*:entry
-/flag == 1/
+syscall::close:return
+/flag == 1 && self->time_in /
 {
-  flag = 0;
+  @total_time[opp_num] = sum  (timestamp - self->time_in );
+  @wall_time[opp_num] = sum(timestamp - wall_time_in);
+  self->time_in = 0;
+  wall_time_in = 0;
   opp_num = opp_num + 1;
+  flag = 0;
 }
 
 dtrace:::END{
-  printf("%-10s %10s %10s %10s %10s %10s\n","#opp", "Time elapsed", "T. Wr. KB", "T. Rd. KB", "#fdsync", "#lseek");
-  printa("%-10d %10@d %10@d %10@d %10@d\n", @total_time , @total_w_size, @total_r_size, @total_fdsync, @lseek_offset );
+  printf("%-10s %20s %20s %20s %20s %20s %20s\n","#opp", "W.T. elapsed", "Syscall T. elapsed", "T. Wr. KB", "T. Rd. KB", "#fdsync", "#lseek");
+  printa("%-10d %20@d %20@d %20@d %20@d %20@d\n", @wall_time, @total_time , @total_w_size, @total_r_size, @total_fdsync, @lseek_offset );
   printf("\n\n Lseek offset analysis:\n");
   printa(@lseek_offset);
 }
--- run_dtrace_disk.sh
+++ run_dtrace_disk.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-      dtrace -s dtrace_disk/open_close_write_lseek.d -c "/opt/csw/bin/iozone -+u -R -i 0 -S 20480 -s 10m -b /export/home/a57816/ESC_ACTIVE_BENCHMARKING_HOME/teste_write.xls -l 1 -u 1" >> dtrace_output_write_new.itxt
+      dtrace -s dtrace_disk/open_close_write_lseek.d -c "/opt/csw/bin/iozone -+u -R -i 0 -S 20480 -s 39g -b /export/home/a57816/ESC_ACTIVE_BENCHMARKING_HOME/teste_write.xls -l 1 -u 1" >> dtrace_output_write_new_26_april.txt
 
