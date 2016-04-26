c33da32936526af36262a9d52406aa0ad19e5a0d
filipecosta90 <filipecosta.90@gmail.com>
2016-04-26 06:59:19 +0100

    added NFS results

---
+++ run_NFS.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+      dtrace -s dtrace_disk/open_close_read_lseek.d -c "/opt/csw/bin/iozone -+u -i 0 -S 20480 -s 1g -l 1 -u 1 -F /share/jade/a57816/iozone.DUMMY.0" >> dtrace_output_write_new_26_april_1gb_WRITE_NFS.txt
+      
+
